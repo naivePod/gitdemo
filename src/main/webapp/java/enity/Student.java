1fd9c0a888dba40aa66da9e5f27a1999e0c3c19e
lgs <1105704860@qq.com>
2018-05-22 17:22:25 +0800

    new add

--- src/main/webapp/java/enity/Student.java
+++ src/main/webapp/java/enity/Student.java
@@ -1,4 +1,13 @@
 package enity;
 
 public class Student {
+    private long sId;
+
+    public long getsId() {
+        return sId;
+    }
+
+    public void setsId(long sId) {
+        this.sId = sId;
+    }
 }
